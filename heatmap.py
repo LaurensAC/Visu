df9a91297b4243526a55bd9deace468f0bc61026
Freek-R <33570091+Freek-R@users.noreply.github.com>
2018-05-29 17:35:48 +0200

    preliminary version. test git connection

--- heatmap.py
+++ heatmap.py
@@ -1,4 +1,6 @@
-from pre import *
+from sys import getsizeof
+
+# from pre import *
 
 from math import pi
 import pandas as pd
@@ -14,56 +16,59 @@ from bokeh.models import (
     ColorBar,
 )
 from bokeh.plotting import figure
+import json
 
 
-# setup data
-def create_heat_matrix(og_df, city, count_duration):
-    # initialize empty matrix
-    xdim = stimuli_meta[city]['x_dim']
-    ydim = stimuli_meta[city]['y_dim']
-    result = np.zeros((int(ydim), int(xdim)))
-
-    # select rows from the original dataframe
-    df_city = og_df[(og_df['StimuliName'] == city) & (og_df['FixationOOB'] == False)]
-
-    if count_duration:  # sum over the fixation durations for every pixel on the map
-        for row in df_city.iterrows():
-            x = row[1]['MappedFixationPointX']
-            y = row[1]['MappedFixationPointY']
-            result[y][x] += row[1]['FixationDuration']
-    else:  # count of the number of fixations for every pixel on the map
-        for row in df_city.iterrows():
-            x = row[1]['MappedFixationPointX']
-            y = row[1]['MappedFixationPointY']
-            result[y][x] += 1
-    return result
+def run_heatmap(city_name, resolution_resize=10):
+    read_main_df(city_name)
 
 
-def create_heat_df(og_df, city, count_duration):
-    fixation_counts = create_heat_matrix(og_df, city, count_duration)
-    heat_dict = {'x': [], 'y': [], 'heat': []}  #
+def preprocess_dataframe():
 
-    for i in range(len(fixation_counts)):
-        for j in range(len(fixation_counts[i])):
-            heat_dict['x'].append(j)
-            heat_dict['y'].append(i)
-            heat_dict['heat'].append(fixation_counts[i][j])
-    return pd.DataFrame(data=heat_dict, columns=['x', 'y', 'heat'])
+# TODO
+def set_resolution(resolution_resize):
 
 
-def show_heatmap(heat_df, title):
+def show_heatmap(stimulus):
+    df = pd.read_csv("up.csv")
+    with open("stimuli_meta.json", 'r') as f:
+        stimuli_meta = json.load(f)
+
+    # key: city name, value: (x,y) coordinates
+    map_sizes = {extractCityName(stim): (stimuli_meta[stim]['x_dim'], stimuli_meta[stim]['y_dim'])
+                 for stim in df['StimuliName'].unique()}
+
+    """
+    og_df: original dataframe with alle data
+    city_name: city name as string
+    rounds_coords_to: divide all coordinates by this number to have less unique coordinate pairs 
+    count_duration: True if fixation duration is important, False if only counting number of fixations
+    smoothing_range: the value of a pixel(block) is the the mean value of the submatrix containing this many pixel(blocks) 
+                    to every side of the concerning pixel(block)
+    weight_centre: the value of the centre pixel(block) is multiplied by this number. Only makes sense when smoothing_range > 0 
+    """
+    heatCountsDf = create_heat_df(og_df=df,
+                                  city_name=stimulus,
+                                  round_coords_to=100,
+                                  count_duration=False,
+                                  smoothing_range=5,
+                                  weight_centre=4)
+
     # this is the colormap from the original NYTimes plot
-    colors = ["#75968f", "#a5bab7", "#c9d9d3", "#e2e2e2", "#dfccce", "#ddb7b1", "#cc7878", "#933b41", "#550b1d"]
-    mapper = LinearColorMapper(palette=colors, low=0, high=heat_df.heat.max())
+    colors = ["#1a9850", "#66bd63", "#a6d96a", "#d9ef8b", "#ffffbf", "#fee08b", "#fdae61", "#f46d43", "#d73027"]
+    mapper = LinearColorMapper(palette=colors, low=0, high=heatCountsDf.heat.max())
 
-    source = ColumnDataSource(heat_df)
+    source = ColumnDataSource(heatCountsDf)
 
     TOOLS = "hover,save,pan,box_zoom,reset,wheel_zoom"
 
-    p = figure(title=title,
-               x_range=range(len(df['x'])), y_range=range(len(df['y'])),
-               x_axis_location="above", plot_width=len(df['x']), plot_height=len(df['y']),
-               tools=TOOLS, toolbar_location='below')
+    xdim = map_sizes[stimulus][0]
+    ydim = map_sizes[stimulus][1]
+    p = figure(title='{} count'.format(stimulus),
+               # x_range=(0, xdim), y_range=(0, ydim),
+               x_axis_location="above",
+               # plot_width=xdim, plot_height=ydim,
+               tools=TOOLS)
 
     p.grid.grid_line_color = None
     p.axis.axis_line_color = None
@@ -72,7 +77,8 @@ def show_heatmap(heat_df, title):
     p.axis.major_label_standoff = 0
     p.xaxis.major_label_orientation = pi / 3
 
-    p.rect(x="x", y="y", width=1, height=1,
+    p.rect(x="x", y="y",
+           width=1, height=1,
            source=source,
            fill_color={'field': 'heat', 'transform': mapper},
            line_color=None)
@@ -83,98 +89,125 @@ def show_heatmap(heat_df, title):
                          label_standoff=6, border_line_color=None, location=(0, 0))
     p.add_layout(color_bar, 'right')
 
+    # hover tool
     p.select_one(HoverTool).tooltips = [
-        ('date', '@Month @Year'),
-        ('rate', '@rate%'),
+        ('x, y', '@x, @y'),
+        ('#', '@heat%'),
     ]
 
     show(p)  # show the plot
 
 
-# -----
-city = "20_Paris_S1.jpg"
+def create_heat_df(og_df, city_name, round_coords_to=1, count_duration=False, smoothing_range=0, weight_centre=1):
+    # create matrix with fixation counts. smoothing may be applied
+    if smoothing_range <= 0:
+        fixation_counts = create_heat_matrix(og_df, city_name, count_duration, round_coords_to)
+    else:  # smoothing_range > 0
+        fixation_counts = create_heat_matrix(og_df, city_name, count_duration, round_coords_to)
+        fixation_counts = applySmoothing(fixation_counts, smoothing_range, weight_centre)
 
-# only count the number of times people look at a spot
-countDuration = False
-heatCountsDf = create_heat_df(df, city, countDuration)
-show_heatmap(heatCountsDf, city + ' count')
+    heat_dict = {'x': [], 'y': [], 'heat': []}  #
+
+    for i in range(len(fixation_counts)):
+        for j in range(len(fixation_counts[i])):
+            heat_dict['x'].append(j)
+            heat_dict['y'].append(i)
+            heat_dict['heat'].append(fixation_counts[i][j])
+    print("dataframe completed")
+    return pd.DataFrame(data=heat_dict, columns=['x', 'y', 'heat'])
+
+
+def create_heat_matrix(og_df, city_name, count_duration=False, round_coords_to=1):
+    # initialize empty matrix
+    xdim_og = map_sizes[city_name][0]
+    ydim_og = map_sizes[city_name][1]
+    xdim = int(xdim_og / round_coords_to) + 1
+    ydim = int(ydim_og / round_coords_to) + 1
+    result = np.zeros((int(ydim), int(xdim)))
+
+    # key: city name, value: list of strings from StimuliName that contain the city name
+    map_names = {extractCityName(stim): [x for x in og_df['StimuliName'].unique() if extractCityName(stim) in x]
+                 for stim in og_df['StimuliName'].unique()}
+
+    # select rows from the original dataframe
+    df_city = og_df[(og_df['StimuliName'].isin(map_names[city_name])) & (og_df['FixationOOB'] == False)]
+
+    if count_duration:  # sum over the fixation durations for every pixel on the map
+        for row in df_city.iterrows():
+            x = int(row[1]['MappedFixationPointX'] / round_coords_to)
+            y = int(row[1]['MappedFixationPointY'] / round_coords_to)
+            result[y][x] += row[1]['FixationDuration']
+    else:  # count of the number of fixations for every pixel on the map
+        for row in df_city.iterrows():
+            x = int(row[1]['MappedFixationPointX'] / round_coords_to)
+            y = int(row[1]['MappedFixationPointY'] / round_coords_to)
+            result[y][x] += 1
+    print('matrix completed')
+    return result
+
+
+def applySmoothing(matrix, smoothing_range, weight_centre):
+    smoothed_matrix = []
+    print('total length:', len(matrix))
+    for i in range(len(matrix)):  # y
+        if i % 100 == 0:
+            print(i, end=' ')
+        new_row = []
+        for j in range(len(matrix[i])):  # x
+            matrix_subset = subsetMatrix(matrix, j, i, smoothing_range)
+
+            # calculate mean value of the elements in the submatrix, giving extra weight to the centre
+            N = matrix_subset.shape[0] * matrix_subset.shape[1]
+            sum_matrix = matrix_subset.sum()  # including the centre value
+            sum_matrix += (weight_centre - 1) * matrix[i][j]  # add extra weight to the centre, excluding the one we've already added before
+
+            new_row.append(sum_matrix / (N - 1 + weight_centre))  # divide by N including the extra additions of weight_centre
+        smoothed_matrix.append(new_row)
+    print('\nsmoothing completed')
+    return smoothed_matrix
+
+
+def subsetMatrix(matrix, x, y, smoothing_range):
+    x_min = max(x - smoothing_range, 0)
+    x_max = min(x + smoothing_range, len(matrix[y]))
+    y_min = max(y - smoothing_range, 0)
+    y_max = min(y + smoothing_range, len(matrix))
+    return matrix[y_min: y_max, x_min: x_max]
+
+
+def print_vars(var_dict):
+    variables = ["{}: {}".format(key, getsizeof(value)) for key, value in var_dict.items()]
+    for x in variables:
+        print(x)
+    sizes = [getsizeof(x) for x in var_dict.values()]
+    print("total space: {}".format(sum(sizes)))
+
+
+# useful (global) dictionaries
+def extractCityName(stimuli_name):
+    return stimuli_name.rsplit('_', 1)[0].split('_', 1)[-1]
+
+
+stimulus = '01_Antwerpen_S2.jpg'
+show_heatmap(stimulus)
+
+
+
 
 '''
 # Add up all fixation durations each time someone looks at a spot
-countDuration = True
-heatDurationsDf = heat_df(df, city, countDuration)
+heatDurationsDf = create_heat_df(og_df=df,
+                                city_name=cityName,
+                                round_coords_to=1,
+                                count_duration=True,
+                                smoothing_range=5,
+                                weight_centre=4)
+show_heatmap(heatDurationsDf, '{} duration'.format(cityName))
 
 # mean fixation duration per look
 heatAvgDf = heatDurationsDf.copy()
 heatAvgDf['heat'] = [heatDurationsDf['heat'][i] / heatCountsDf['heat'][i]
                      if heatCountsDf['heat'][i] > 0 else 0
                      for i in range(len(heatDurationsDf['heat']))]
-'''
-
-'''
-EXAMPLE CODE
-
-from math import pi
-import pandas as pd
-
-from bokeh.io import show
-from bokeh.models import (
-    ColumnDataSource,
-    HoverTool,
-    LinearColorMapper,
-    BasicTicker,
-    PrintfTickFormatter,
-    ColorBar,
-)
-from bokeh.plotting import figure
-from bokeh.sampledata.unemployment1948 import data
-
-data['Year'] = data['Year'].astype(str)
-data = data.set_index('Year')
-data.drop('Annual', axis=1, inplace=True)
-data.columns.name = 'Month'
-
-years = list(data.index)
-months = list(data.columns)
-
-# reshape to 1D array or rates with a month and year for each row.
-df = pd.DataFrame(data.stack(), columns=['rate']).reset_index()
-
-# this is the colormap from the original NYTimes plot
-colors = ["#75968f", "#a5bab7", "#c9d9d3", "#e2e2e2", "#dfccce", "#ddb7b1", "#cc7878", "#933b41", "#550b1d"]
-mapper = LinearColorMapper(palette=colors, low=df.rate.min(), high=df.rate.max())
-
-source = ColumnDataSource(df)
-
-TOOLS = "hover,save,pan,box_zoom,reset,wheel_zoom"
-
-p = figure(title="US Unemployment ({0} - {1})".format(years[0], years[-1]),
-           x_range=years, y_range=list(reversed(months)),
-           x_axis_location="above", plot_width=900, plot_height=400,
-           tools=TOOLS, toolbar_location='below')
-
-p.grid.grid_line_color = None
-p.axis.axis_line_color = None
-p.axis.major_tick_line_color = None
-p.axis.major_label_text_font_size = "5pt"
-p.axis.major_label_standoff = 0
-p.xaxis.major_label_orientation = pi / 3
-
-p.rect(x="Year", y="Month", width=1, height=1,
-       source=source,
-       fill_color={'field': 'rate', 'transform': mapper},
-       line_color=None)
-
-color_bar = ColorBar(color_mapper=mapper, major_label_text_font_size="5pt",
-                     ticker=BasicTicker(desired_num_ticks=len(colors)),
-                     formatter=PrintfTickFormatter(format="%d%%"),
-                     label_standoff=6, border_line_color=None, location=(0, 0))
-p.add_layout(color_bar, 'right')
-
-p.select_one(HoverTool).tooltips = [
-     ('date', '@Month @Year'),
-     ('rate', '@rate%'),
-]
-
-show(p)      # show the plot
+show_heatmap(heatAvgDf, '{} average'.format(cityName))
 '''
