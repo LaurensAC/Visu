22a13ce7233f453f912e3066fd9e69b7b61d325e
LaurensAC <laurenscastelijns@gmail.com>
2018-06-12 12:35:10 +0200

    removed column parameter

--- app_path/orders.py
+++ app_path/orders.py
@@ -3,15 +3,11 @@ import math
 import scipy.cluster.hierarchy as sch
 
 # TODO Laurens, Neil:
-# Wats met 'column'? (kan het zonder?) (non-deterministisch?)
-# >>> zou chill zijn als alle functies dezelfde input krijgen e.g. de matrix
-
 # Geef de functies namen waar de gebruiker ('client') wat aan heeft
 
 
 #uses squared distance matrix + double centering + SVD
-def seriationMDS(matrix, column):
-    np.random.seed(0)
+def seriationMDS(matrix):
     lengte = len(matrix["count"])
     wortellengte = int(math.sqrt(lengte))
     for i in range(0, lengte-1):
@@ -26,16 +22,35 @@ def seriationMDS(matrix, column):
     B = -0.5 * (J_c.dot(A)).dot(J_c)
 
     U, E, V = np.linalg.svd(B)
-    U = [row[column] for row in U]
+    U = [row[0] for row in U]
 
     U, names = zip(*sorted(zip(U, names)))
     U, names = (list(t) for t in zip(*sorted(zip(U, names))))
     return names
 
+def seriationMDS2(matrix):
+    lengte = len(matrix["count"])
+    wortellengte = int(math.sqrt(lengte))
+    for i in range(0, lengte-1):
+        matrix["count"][i] = (1 - matrix["count"][i]) ** 2
+    A = np.array(matrix["count"]).reshape((wortellengte, wortellengte))
+    names = np.array(matrix["xname"]).reshape((wortellengte, wortellengte))
+    names = [row[0] for row in names]
+    n = A.shape[0]
+    J_c = 1. / n * (np.eye(n) - 1 + (n - 1) * np.eye(n))
+
+    # perform double centering
+    B = -0.5 * (J_c.dot(A)).dot(J_c)
+
+    U, E, V = np.linalg.svd(B)
+    U = [row[1] for row in U]
+
+    U, names = zip(*sorted(zip(U, names)))
+    U, names = (list(t) for t in zip(*sorted(zip(U, names))))
+    return names
 
 #only uses SVD
-def seriationMDS3(matrix, column):
-    np.random.seed(0)
+def SVDcolumn1(matrix):
     lengte = len(matrix["count"])
     wortellengte = int(math.sqrt(lengte))
 
@@ -45,15 +60,31 @@ def seriationMDS3(matrix, column):
 
 
     U, E, V = np.linalg.svd(A)
-    U = [row[column] for row in U]
+    U = [row[0] for row in U]
+
+    U, names = zip(*sorted(zip(U, names)))
+    U, names = (list(t) for t in zip(*sorted(zip(U, names))))
+    return names
+
+#only uses SVD
+def SVDcolumn2(matrix):
+    lengte = len(matrix["count"])
+    wortellengte = int(math.sqrt(lengte))
+
+    A = np.array(matrix["count"]).reshape((wortellengte, wortellengte))
+    names = np.array(matrix["xname"]).reshape((wortellengte, wortellengte))
+    names = [row[0] for row in names]
+
+
+    U, E, V = np.linalg.svd(A)
+    U = [row[1] for row in U]
 
     U, names = zip(*sorted(zip(U, names)))
     U, names = (list(t) for t in zip(*sorted(zip(U, names))))
     return names
 
 #uses distance matrix + SVD
-def seriationMDS5(matrix, column):
-    np.random.seed(0)
+def distanceSVDcolumn1(matrix):
     lengte = len(matrix["count"])
     wortellengte = int(math.sqrt(lengte))
     for i in range(0, lengte-1):
@@ -64,7 +95,25 @@ def seriationMDS5(matrix, column):
 
 
     U, E, V = np.linalg.svd(A)
-    U = [row[column] for row in U]
+    U = [row[0] for row in U]
+
+    U, names = zip(*sorted(zip(U, names)))
+    U, names = (list(t) for t in zip(*sorted(zip(U, names))))
+    return names
+
+#uses distance matrix + SVD
+def distanceSVDcolumn2(matrix):
+    lengte = len(matrix["count"])
+    wortellengte = int(math.sqrt(lengte))
+    for i in range(0, lengte-1):
+        matrix["count"][i] = (1 - matrix["count"][i])
+    A = np.array(matrix["count"]).reshape((wortellengte, wortellengte))
+    names = np.array(matrix["xname"]).reshape((wortellengte, wortellengte))
+    names = [row[0] for row in names]
+
+
+    U, E, V = np.linalg.svd(A)
+    U = [row[1] for row in U]
 
     U, names = zip(*sorted(zip(U, names)))
     U, names = (list(t) for t in zip(*sorted(zip(U, names))))
